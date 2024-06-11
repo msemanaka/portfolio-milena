f19e0357bbd700c74f70610d445a41930c9220e4
hunterblp <milena.sforte@gmail.com>
2024-06-11 00:29:54 -0300

    Inicializando projeto + Construção Navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,29 @@
+import { MenuSharp } from "react-ionicons"
+
+function Navbar() {
+    return (
+        <>
+            <nav className="border-[3px] border-grape m-5 flex justify-between items-center mx-auto w-[92%] ">
+                <div className="font-semibold text-5xl px-3 py-1 border-r-[3px]">
+                    SEMANAKA
+                </div>
+
+                <div className="font-semibold text-2xl px-5 py-1 md:static absolute md:min-h-fit min-h-[60vh] left-0 top-[-100%] md:w-auto w-full flex items-center ">
+                    <ul className="flex md:flex-row flex-col md:items-center md:gap-[4VW] gap-8">
+                        <a className="" href="#">HOME</a>
+                        <a className="" href="#">SOBRE</a>
+                        <a className="" href="#">PROJETOS</a>
+
+                    </ul>
+                </div>
+                <div>
+                    <div className="font-semibold text-2xl px-3 py-3 border-l-[3px]">
+                        <a href="#">CONTATO</a>          
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      'tea-green': '#CCFFD1',
+      'grape': '#662EAB',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
